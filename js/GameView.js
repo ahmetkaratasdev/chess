ab1c9571e3c9df18fd36155d0463e3206f4f726b
MacBook Air <macbookair@Ahmets-Air.modem>
2021-01-31 17:18:29 +1100

    modified files

--- js/GameView.js
+++ js/GameView.js
@@ -3,7 +3,6 @@ export default class gameView {
     constructor(root) {
         this.root = root;
         this.root.innerHTML = `
-<<<<<<< HEAD
             <table class="chess-board">
             <tbody>
                 <tr>
@@ -123,87 +122,3 @@ export default class gameView {
 <div class="board">
 <div class="board__tile" data-index="0"></div>
 </div>  */}
-=======
-
-            <div class="header">
-                <div class="header__turn">
-                </div>
-                <div class="header__status">
-                    In Progress
-                </div>
-                <button type="button" class="header__refresh">
-                    <i class="material-icons">refresh</i>
-                </button>
-            </div>
-
-            <div class="board">
-                <div class="board__tile" data-index="0"></div>
-                <div class="board__tile" data-index="1"></div>
-                <div class="board__tile" data-index="2"></div>
-                <div class="board__tile" data-index="3"></div>
-                <div class="board__tile" data-index="4"></div>
-                <div class="board__tile" data-index="5"></div>
-                <div class="board__tile" data-index="6"></div>
-                <div class="board__tile" data-index="7"></div>
-                <div class="board__tile" data-index="8"></div>
-            </div>
-
-        `;
-
-        this.onTileClick = undefined;
-        this.onRestartClick = undefined;
-        this.root.querySelectorAll(".board__tile").forEach(tile => {
-            tile.addEventListener("click", () => {
-                if (this.onTileClick) {
-                    this.onTileClick(tile.dataset.index);
-                }
-            });
-
-        });
-
-        this.root.querySelector(".header__refresh").addEventListener("click", () => {
-            if (this.onRestartClick) {
-                this.onRestartClick();
-            }
-        });
-
-        
-        
-    }
-    update(game) {
-        this.updateTurn(game);
-        this.updateStatus(game);
-        this.updateBoard(game);
-    }
-
-    updateTurn(game) {
-        this.root.querySelector(".header__turn").textContent = `${game.turn}'s turn`;
-    }
-
-    updateStatus(game) {
-        let status = "In Progress";
-        if (game.findWinningCombination()) {
-            status = `${game.turn} is the Winner!`;
-            
-        } else if (!game.isInProgress()) {
-            status = "It's a tie!";
-        }
-        this.root.querySelector(".header__status").textContent = status;
-    }
-
-    updateBoard(game) {
-        const winningComb = game.findWinningCombination();
-        for (let i = 0; i < game.board.length; i++) {
-            const tile = this.root.querySelector(`.board__tile[data-index="${i}"]`);
-            
-            tile.classList.remove("board__tile--winner");
-            tile.textContent = game.board[i];
-
-            if (winningComb && winningComb.includes(i)) {
-                tile.classList.add("board__tile--winner");
-            }
-        }
-    }
-
-}
->>>>>>> 0695864dc9022b2cea3e52751c4a1545df9bce6a
