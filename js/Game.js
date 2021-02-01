4fea8a4e4f5c5564a8db61d4f1c5fc831d336724
MacBook Air <macbookair@Ahmets-Air.modem>
2021-02-01 18:38:46 +1100

    dabbled around with trying to convert the structure of chessboard from C to javascript

--- js/Game.js
+++ js/Game.js
@@ -1,17 +1,73 @@
 // Going to represent a single game of chess
+const NO = 0;
+const YES = 1;
+const SIZE = 8;
+
+const chessPiece = {
+    NONE: 0,
+    PAWN: 1,
+    ROOK: 2,
+    KNIGHT: 3,
+    BISHOP: 4,
+    QUEEN: 5,
+    KING: 6
+};
+
+const colour = {
+    EMPTY: 0,
+    WHITE: 1,
+    BLACK: 2
+};
+
+Object.freeze(chessPiece);
+Object.freeze(colour);
+
+class canEmpassant {
+    constructor() {
+        this.answer = NO;
+        this.withRow = -1;
+        this.withCol = -1;
+    }
+}
+
+class board {
+    constructor() {
+        this.hasPieceMoved = NO;
+        this.piece = chessPiece.NONE;
+        this.colour = colour.EMPTY;
+        this.empassant = new canEmpassant;
+        
+    }
+;}
+
 export default class Game {
     // A constructor is used to initialise objects
     constructor() {
         this.turn = "White";
-        
-        let chessboard = [];
+        this.chessboard = [
+
+            [2, 3, 4, 5, 6, 4, 3, 2],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [2, 3, 4, 5, 6, 4, 3, 2]
+        ];
+        console.log(colour.WHITE);
+        console.log(chessPiece);
         for (let row = 0; row < 8; row++) {
-            chessboard[row] = [];
             for (let col = 0; col < 8; col++) {
-                chessboard[row][col] = 0;
+                if (row == 0 || row == 1) {
+                    chessboard[row][col].colour = colour.WHITE;
+                } else if (row == SIZE - 2 || row == SIZE - 1) {
+                    chessboard[row][col].colour = colour.BLACK;
+                } else {
+                    chessboard[row][col].colour = colour.EMPTY;
+                }
             }
         }
-        this.chessboard = chessboard;
     }
 }
 
